9fb337d58a2f32b1f7eebbb260777cd4d2a1f351
Antony Male <antony.male@gmail.com>
2012-11-21 18:12:18 +0000

    Correct some bits of the TeamCity driver

--- lib/asbo/repos/teamcity.rb
+++ lib/asbo/repos/teamcity.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-require 'uri'
 require 'nokogiri'
 
 module ASBO::Repo
@@ -8,10 +7,8 @@ module ASBO::Repo
 
     def initialize(workspace_config, source, package, type, version)
       url = source['url']
-      parsed = URI::parse(url)
-      user, pass = parsed.user, parsed.password
-      # Hacky, but CBA to reconstruct URl
-      url.gsub!("#{user}:#{pass}@", '')
+      user = source['username']
+      pass = source['password']
       project = source['project']
 
       teamcity_package = source['package'] ? workspace_config.resolve_config_vars(source['package'], {}, package) : package
@@ -24,11 +21,11 @@ module ASBO::Repo
     def fetch_bt(url, project, package, user, pass)
       projects_url = url + '/httpAuth/app/rest/projects'
       log.debug "Looking for projects: #{projects_url}"
-      node = Nokogiri::HTML(open(projects_url, :http_basic_authentication => [user, pass])).css('project').first{ |x| x['name'] == project }
+      node = Nokogiri::XML(open(projects_url, :http_basic_authentication => [user, pass])).css('project').find{ |x| x['name'] == project }
       raise "Can't find teamcity project #{project}" unless node
       project_url = url + node['href']
       log.debug "Looking for BT: #{project_url}"
-      node = Nokogiri::HTML(open(project_url, :http_basic_authentication => [user, pass])).css('buildtypes').first{ |x| x['name'] == package }
+      node = Nokogiri::XML(open(project_url, :http_basic_authentication => [user, pass])).css('buildType').find{ |x| x['name'] == package }
       raise "Can't find teamcity package #{package}" unless node
       node['id']
     end
