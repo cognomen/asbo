4b71f8fbbdf5324bb45856a98248a950ea5bb78a
Mark Ferry <mark@markferry.net>
2012-11-29 12:08:57 +0000

    File repo publishing

--- lib/asbo/repos/file.rb
+++ lib/asbo/repos/file.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module ASBO::Repo
   class File
     include ASBO::Logger
@@ -9,13 +11,34 @@ module ASBO::Repo
         'version' => version,
       }
       path = workspace_config.resolve_vars_in_str(path, vars)
-      @path = ::File.expand_path(::File.join(workspace_config.workspace, path)) << '.zip'
+      @path = ::File.expand_path(::File.join(workspace_config.workspace, path))
       log.debug "Got path: #{@path}"
     end
 
     def download
       raise AppError,  "Can't find package source #{@source}" unless ::File.file?(@path)
       @path
+    end
+
+    def publish(file, overwrite=false)
+      log.debug "Publishing #{@path}"
+
+      begin
+       FileUtils.mkdir_p(::File.dirname(@path))
+      rescue SystemCallError => e
+        raise ASBO::AppError, "Failed to create dir: #{e.message}"
+      end
+
+      exists = ::File.file?(::File.basename(@path))
+      raise ASBO::AppError, "File #{@path} already exists. Use the appropriate flag to force overwriting" if exists && !overwrite
+
+      log.debug "Uploading..."
+      begin
+        FileUtils.cp(file, ::File.basename(@path))
+      rescue SystemCallError => e
+        raise ASBO::AppError, "Failed to upload file #{@path}: #{e.message}"
+      end
+      log.info "Uploaded #{@path}"
     end
   end
 end
